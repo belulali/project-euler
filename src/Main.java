b828d9e5a805e04491d9e958b85d142be6003baa
Pamela <pamela.nchs@gmail.com>
2014-04-22 16:07:45 -0500

    Added a main method

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by pchavez on 22/04/14.
+ */
+public class Main {
+    public static void main (String[] args) {
+
+        Multiples multiples = new Multiples();
+        System.out.println(multiples.sumMultipleNumbersOfThreeAndFive(1000));
+    }
+}
