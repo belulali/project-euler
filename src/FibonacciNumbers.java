136a8a6e1028dd315a5ca394f4f8aa5ea8ee9e48
Pamela <pamela.nchs@gmail.com>
2014-04-23 12:52:35 -0500

    Added a method to generate the Fibonacci numbers, and a method to sum even numbers

--- src/FibonacciNumbers.java
+++ src/FibonacciNumbers.java
@@ -1,8 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by pchavez on 23/04/14.
  */
 public class FibonacciNumbers {
     public boolean isEven(int number) {
         return number % 2 == 0;
+    }
+
+    public List<Integer> getFibonacciNumbers(int term) {
+
+        List fibonacciNumbers = new ArrayList();
+        int anterior = 1;
+        int actual = 2;
+
+        for(int i = 1; i < term; i++){
+            if(i <= 2){
+                fibonacciNumbers.add(i);
+            }else {
+
+                int temporal = anterior + actual;
+                fibonacciNumbers.add(temporal);
+                anterior = actual;
+                actual = temporal;
+            }
+
+            if(actual + anterior >= term){
+                break;
+            }
+        }
+        return fibonacciNumbers;
+    }
+
+    public int sumEvenFibonacciNumbers(int term) {
+        List<Integer> fibonacciNumbers = getFibonacciNumbers(term);
+        int result = 0;
+        for(Integer fibonacci : fibonacciNumbers){
+            if(isEven(fibonacci)){
+                result += fibonacci;
+            }
+        }
+        return result;
     }
 }
--- test/FibonacciNumbersTest.java
+++ test/FibonacciNumbersTest.java
@@ -1,6 +1,9 @@
 import org.junit.Test;
 
+import java.util.List;
+
 import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Created by pchavez on 23/04/14.
@@ -19,4 +22,19 @@ public class FibonacciNumbersTest {
         assertEquals(false, fibonacciNumbers.isEven(3));
     }
 
+    @Test
+    public void shouldReturnTheFibonacciNumbersUntilFive() {
+        FibonacciNumbers fibonacciNumbers = new FibonacciNumbers();
+        List<Integer> actualFibonacciNumbers = fibonacciNumbers.getFibonacciNumbers(6);
+        Integer[] expectedFibonacciNumbers = {1, 2, 3, 5};
+        assertArrayEquals(expectedFibonacciNumbers, actualFibonacciNumbers.toArray());
+    }
+
+    @Test
+    public void shouldSumTheEvenNumbersOfTheFibonacciNumbersList() {
+        FibonacciNumbers fibonacciNumbers = new FibonacciNumbers();
+        assertEquals(10, fibonacciNumbers.sumEvenFibonacciNumbers(10));
+
+
+    }
 }
